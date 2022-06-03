381d505ea11e1c47fd67a6bdafccfea76fa8a10e
Francois Poizat <francois.poizat@gmail.com>
2022-06-03 09:22:21 +0200

    Wallpapers added

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+[ -d "./Wallpapers" ] && rm -rf Wallpapers
+cp -R ~/Wallpapers/ .
 [ ! -d "./nvim/" ] && mkdir nvim
 cp ~/.config/nvim/init.vim nvim/
 [ ! -d "./i3/" ] && mkdir i3
 cp ~/.config/i3/config i3/
 [ -d "./polybar/" ] && rm -rf polybar
-cp -R ~/.config/polybar/ polybar/
+cp -R ~/.config/polybar/ .
 
